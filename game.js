2285d1e793ac79ffced34cb29e9f8622a801649a
Sara Martinez <s_martinez_1990@outlook.com>
2019-10-26 00:27:19 -0400

    footer added

--- game.js
+++ game.js
@@ -6,26 +6,6 @@ let lose=0;
 let userGuesses=[];
 let guesses=10;
 
-document.onkeydown = () => {
-    guesses --;
-    let letter = event.key.toLowerCase();
-    userGuesses.push(letter);
-    guessesLeft();
-    updateUserGuesses();
-
-        if(letter === computerGuess) {
-            win ++;
-            document.querySelector("#wins").innerHTML = "Wins! " + win;
-            reset();
-        }
-        if (guesses === 0) {
-            lose++;
-            document.querySelector("#losses").innerHTML = "Losses! " + lose;
-            reset();
-        }
-
-    }
-
 reset = () => {
     document.querySelector("#guessesLeft").innerHTML = "You have " + guesses + " guesses left!";
     document.querySelector("#wins").innerHTML = "Wins! " + win;
@@ -54,7 +34,7 @@ const guessesLeft = () => {
 
 const computerChoice = () => {
      computerGuess =  computerChoices[Math.floor(Math.random()*computerChoices.length)]; {
-            console.log(computerGuess);
+            console.log("Computer chose: " + computerGuess);
         }
     }
 
@@ -62,4 +42,22 @@ const updateUserGuesses = () => {
     document.querySelector("#userGuesses").innerHTML = "You have already guessed: " + userGuesses.join(", ");
 }
 
+document.onkeydown = () => {
+    guesses --;
+    let letter = event.key.toLowerCase();
+    userGuesses.push(letter);
+    guessesLeft();
+    updateUserGuesses();
 
+        if(letter === computerGuess) {
+            win ++;
+            document.querySelector("#wins").innerHTML = "Wins! " + win;
+            reset();
+        }
+        if (guesses === 0) {
+            lose++;
+            document.querySelector("#losses").innerHTML = "Losses! " + lose;
+            reset();
+        }
+
+    }
